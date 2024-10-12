b6495397a2b9f59d12cfa936105e2847fd811629
heycatch <andreyisback@yandex.ru>
2024-10-13 03:23:35 +0400

    updated README and shell

--- src/shell.d
+++ src/shell.d
@@ -5,44 +5,36 @@ import std.string;
 class Shell {
   string configPath() {
     string name = executeShell("whoami").output.strip();
-    if (name == "root") {
-      return "/root/.config/wg-keys/";
-    }
-    return "/home/" ~ name ~ "/.config/wg-keys/";
+    string path = name == "root" ? "/root/.config/wg-keys/" : "/home/" ~ name ~ "/.config/wg-keys/";
+    return path;
   }
 
   int generateKeys(string path, string priv, string pub) {
-    string command = format(
-      "wg genkey | tee %s/%s | wg pubkey | tee %s/%s",
-      path, priv, path, pub);
-    return executeShell(command).status;
+    return executeShell(format("wg genkey | tee %s/%s | wg pubkey | tee %s/%s", path, priv, path, pub)).status;
   }
 
   string readKey(string key) {
-    string command = format("cat %s", key);
-    return executeShell(command).output.strip();
+    return executeShell(format("cat %s", key)).output.strip();
   }
 
   string lsDir(string path) {
-    string command = format("ls %s", path);
-    return executeShell(command).output.strip();
+    return executeShell(format("ls %s", path)).output.strip();
   }
 
   int restartServer(string name) {
-    string command = format("wg-quick down %s && wg-quick up %s", name, name);
-    return executeShell(command).status;
+    return executeShell(format("wg-quick down %s && wg-quick up %s", name, name)).status;
   }
 
   string getEthInterface() {
+    string eth = "";
     string command = executeShell("ip addr | grep '2: '").output.strip();
-    string res = "";
 
     for (int i = 3; i < command.split("").length; i++) {
       if (command[i] == ':') break;
-      res ~= command[i];
+      eth ~= command[i];
     }
 
-    return res;
+    return eth;
   }
 
   string getIpAddress() {
