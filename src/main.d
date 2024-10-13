9b9c9a4befa21d1057bcb582f2a7d1573ed15059
heycatch <andreyisback@yandex.ru>
2024-10-13 17:07:37 +0400

    adapted the readability of the code by adding assert

--- src/main.d
+++ src/main.d
@@ -23,49 +23,43 @@ void main(string[] args) {
     folder.createConfigDir(config ~ "temp");
 
     int keys = sh.generateKeys(config ~ "server", key.priv, key.pub);
-    if (keys == 0) {
-      writeln("ok... keys genreated");
-      // FIXME: change config~temp/->WGDIR
-      folder.createServerConfigFile(
-        config ~ "temp/", args[2],
-        sh.readKey(config ~ "server/" ~ key.priv),
-        sh.getEthInterface());
-      writeln("ok... config file created");
-    } else {
-      writefln("generate keys error code: %d", keys);
-    }
+    assert(keys == 0, "key generation error");
+    writeln("ok... keys genreated");
+
+    // FIXME: change config~temp/->WGDIR
+    folder.createServerConfigFile(
+      config ~ "temp/", args[2],
+      sh.readKey(config ~ "server/" ~ key.priv),
+      sh.getEthInterface());
+    writeln("ok... config file created");
   } else if (args.length == 3 && args[1] == "client") {
     Key key = Key(args[2]);
 
     int keys = sh.generateKeys(config, key.priv, key.pub);
-    if (keys == 0) {
-      // FIXME: change config~temp->WGDIR; config~temp->WGDIR
-      folder.addUser(
-        config ~ "temp/" ~ sh.lsDir(config ~ "temp"),
-        args[2], sh.readKey(config ~ key.pub));
-      writefln("ok... %s added", args[2]);
-      folder.createUserConfigFile(
-        // FIXME: change config~temp/->WGDIR; config~temp->WGDIR
-        Info.SERVERLOCATION, config ~ "temp/" ~ sh.lsDir(config ~ "temp"),
-        sh.readKey(config ~ key.priv),
-        sh.readKey(config ~ "server/publickey.*"), sh.getIpAddress(), Info.SERVERPORT);
-      writefln("ok... %s.conf saved in %s", Info.SERVERLOCATION, folder.TEMPDIR);
+    assert(keys == 0, "key generation error");
+    writeln("ok... keys genreated");
 
-      writeln("restart the server now? y/n");
-      string input = readln();
-      if (input[0..$-1] == "y") {
-        // FIXME: change sh.lsDir(config~temp)->WGDIR
-        int server = sh.restartServer(sh.lsDir(config ~ "temp")[0..$-5]);
-        if (server == 0) {
-          writeln("ok... server restarted");
-        } else {
-          writeln("restart server error code: %d", server);
-        }
-      } else {
-        writeln("don't forget to restart the server");
-      }
+    // FIXME: change config~temp->WGDIR; config~temp->WGDIR
+    folder.addUser(
+      config ~ "temp/" ~ sh.lsDir(config ~ "temp"),
+      args[2], sh.readKey(config ~ key.pub));
+    writefln("ok... %s added", args[2]);
+    folder.createUserConfigFile(
+      // FIXME: change config~temp/->WGDIR; config~temp->WGDIR
+      Info.SERVERLOCATION, config ~ "temp/" ~ sh.lsDir(config ~ "temp"),
+      sh.readKey(config ~ key.priv),
+      sh.readKey(config ~ "server/publickey.*"), sh.getIpAddress(), Info.SERVERPORT);
+    writefln("ok... %s.conf saved in %s", Info.SERVERLOCATION, folder.TEMPDIR);
+
+    writeln("restart the server now? y/n");
+    string input = readln();
+    if (input[0..$-1] == "y") {
+      // FIXME: change sh.lsDir(config~temp)->WGDIR
+      int server = sh.restartServer(sh.lsDir(config ~ "temp")[0..$-5]);
+      assert(server == 0, "error when trying to restart the server");
+      writeln("ok... server restarted");
     } else {
-      writefln("generate keys error code: %d", keys);
+      writeln("don't forget to restart the server");
     }
   } else {
     writeln("wrong args: wghelper [server | client] [name]");
